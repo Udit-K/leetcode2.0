f2258f57e525d6b8bccc3538d73dd02acb7b585c
Udit Khanna <udit@udits-mbp.myfiosgateway.com>
2021-05-05 03:07:31 -0400

    updated solution to SingleNumber

--- src/com/udit/topQuestions/easy/SingleNumber.java
+++ src/com/udit/topQuestions/easy/SingleNumber.java
@@ -4,18 +4,11 @@ import java.util.Arrays;
 
 public class SingleNumber {
     public int singleNumber(int[] nums) {
-        if(nums.length == 1) return nums[0];
+        int ans = 0;
 
-        Arrays.sort(nums);
-        int start = 0;
-
-        while(start<nums.length-1) {
-            if(nums[start] == nums[start+1]) {
-                start +=2;
-            }else {
-                return nums[start];
-            }
+        for(int i=0; i< nums.length;i++) {
+            ans = ans^nums[i];
         }
-        return nums[nums.length-1];
+        return ans;
     }
 }
